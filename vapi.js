c427bcb18dccc1ad7d1e312cf1f42b0aa584370a
HAZSA1 <huul36760@gmail.com>
2026-01-05 01:11:48 +0300

    Update vapi.js

--- vapi.js
+++ vapi.js
@@ -1,16 +1,1 @@
-// Vapi Web SDK Loader
-(function() {
-    var vapiSDK = {
-        run: function(options) {
-            const script = document.createElement('script');
-            script.src = "https://unpkg.com/@vapi-ai/web@latest/dist/vapi.js";
-            script.defer = true;
-            script.onload = function() {
-                const vapi = new Vapi(options.apiKey);
-                vapi.start(options.assistant || options.assistantId);
-            };
-            document.head.appendChild(script);
-        }
-    };
-    window.vapiSDK = vapiSDK;
-})();
+
